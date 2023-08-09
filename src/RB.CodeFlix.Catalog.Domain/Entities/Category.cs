e2e1ae3ac8861578b02067721232ed83c937a9ab
RobsonTrasel <robson.trasel@gmail.com>
2023-08-08 23:21:43 -0300

    feat(entity): add a validation method

--- src/RB.CodeFlix.Catalog.Domain/Entities/Category.cs
+++ src/RB.CodeFlix.Catalog.Domain/Entities/Category.cs
@@ -1,4 +1,6 @@
-﻿namespace RB.CodeFlix.Catalog.Domain.Entities;
+﻿using RB.CodeFlix.Catalog.Domain.Exceptions;
+
+namespace RB.CodeFlix.Catalog.Domain.Entities;
 public class Category
 {
     public Guid Id { get; private set; }
@@ -14,5 +16,21 @@ public class Category
         Description = description;
         IsActive = isActive;
         CreatedAt = DateTime.Now;
+
+        Validate();
+    }
+
+    public void Validate()
+    {
+        if (String.IsNullOrWhiteSpace(Name))
+            throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+        if (Description == null)
+            throw new EntityValidationException($"{nameof(Description)} should not be empty or null");
+        if (Name.Length < 3)
+            throw new EntityValidationException($"{nameof(Name)} should be at least three characters long");
+        if (Name.Length > 255)
+            throw new EntityValidationException($"{nameof(Name)} should be less or equal 255 characters long");
+        if (Description.Length > 10000)
+            throw new EntityValidationException($"{nameof(Description)} should be less or equal 10000 characters long");
     }
 }
