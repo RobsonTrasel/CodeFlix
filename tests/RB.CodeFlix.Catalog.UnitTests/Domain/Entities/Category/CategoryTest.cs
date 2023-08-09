cc9fdd337aa851331d050b3e60b6d807ba4853c8
RobsonTrasel <robson.trasel@gmail.com>
2023-08-08 22:41:54 -0300

    chore(test): create a new test for Instantiate passing isActive paramethers

--- tests/RB.CodeFlix.Catalog.UnitTests/Domain/Entities/Category/CategoryTest.cs
+++ tests/RB.CodeFlix.Catalog.UnitTests/Domain/Entities/Category/CategoryTest.cs
@@ -29,5 +29,33 @@ public class CategoryTest
         Assert.True(category.CreatedAt < dateTimeAfter);
         Assert.True(category.IsActive);
     }
+
+    [Theory(DisplayName = nameof(InstantiateWithActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithActive(bool IsActive)
+    {
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+
+        var dateTimeBefore = DateTime.Now;
+
+        var category = new DomainEntities.Category(validData.Name, validData.Description, IsActive);
+
+        var dateTimeAfter = DateTime.Now;
+
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.Equal(IsActive, category.IsActive);
+    }
 }
 
